b29d0585164fcbb9bfad19d540a356f1ce70a6af
madhan <rajagopalamadhan@gmail.com>
2025-02-04 11:14:42 +0530

    Created Navigations

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 # MySQL Configuration
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'olx'
